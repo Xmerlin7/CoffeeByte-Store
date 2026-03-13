cfccb271458a79559b9f380e7906d5ba46163de4
Xmerlin7 <seifahmedjr7@gmail.com>
2026-03-13 03:18:01 +0200

    feat: implement user management interface to display and manage users

---
+++ admin/user/manage-users.php
@@ -0,0 +1,29 @@
+<?php
+require_once "../../classes/User.php";
+
+$userObj = new User();
+$users = $userObj->getAllUsers();
+?>
+
+<a href="add-user.php">+ Add User</a>
+
+<table border="1">
+    <tr>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Role</th>
+        <th>Actions</th>
+    </tr>
+
+    <?php foreach ($users as $user): ?>
+    <tr>
+        <td><?= $user['name'] ?></td>
+        <td><?= $user['email'] ?></td>
+        <td><?= $user['role'] ?></td>
+        <td>
+            <a href="edit-user.php?id=<?= $user['id'] ?>">Edit</a>
+            <a href="delete-user.php?id=<?= $user['id'] ?>">Delete</a>
+        </td>
+    </tr>
+    <?php endforeach; ?>
+</table>
