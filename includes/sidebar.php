4ca0250fea300aeeb0de2c1f8435e41f106d4312
Mohamed-Fouad-Nassar <mohammednassar740@gmail.com>
2026-03-15 19:29:03 +0200

    ui: add basic styles for dashboard

---
+++ includes/sidebar.php
@@ -0,0 +1,61 @@
+<div class="admin-sidebar">
+    <ul class="sidebar-menu">
+        <li class="menu-title">Main</li>
+        <li>
+            <a href="/admin/dashboard.php">
+                <i class="fa-solid fa-chart-line"></i>
+                Dashboard
+            </a>
+        </li>
+        <hr>
+        <li class="menu-title">Products</li>
+        <li>
+            <a href="/admin/product/manage-products.php">
+                <i class="fa-solid fa-box"></i>
+                Manage Products
+            </a>
+        </li>
+        <li>
+            <a href="/admin/product/create-product.php">
+                <i class="fa-solid fa-plus"></i>
+                Create Product
+            </a>
+        </li>
+        <hr>
+        <li class="menu-title">Orders</li>
+        <li>
+            <a href="/admin/order/manage-orders.php">
+                <i class="fa-solid fa-cart-shopping"></i>
+                Manage Orders
+            </a>
+        </li>
+        <hr>
+        <li class="menu-title">Rooms</li>
+        <li>
+            <a href="/admin/room/manage-rooms.php">
+                <i class="fa-solid fa-door-open"></i>
+                Manage Rooms
+            </a>
+        </li>
+        <li>
+            <a href="/admin/room/add-room.php">
+                <i class="fa-solid fa-plus"></i>
+                Create Room
+            </a>
+        </li>
+        <hr>
+        <li class="menu-title">Users</li>
+        <li>
+            <a href="/admin/room/manage-users.php">
+                <i class="fa-solid fa-users"></i>
+                Manage Users
+            </a>
+        </li>
+        <li>
+            <a href="/admin/room/add-user.php">
+                <i class="fa-solid fa-user-plus"></i>
+                Create User
+            </a>
+        </li>
+    </ul>
+</div>
