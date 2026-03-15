411975966f2aa9637c86ef7f21dea887044103da
Xmerlin7 <seifahmedjr7@gmail.com>
2026-03-16 01:17:23 +0200

    fix: restore admin check in add-user.php

--- admin/user/add-user.php
+++ admin/user/add-user.php
@@ -1,7 +1,7 @@
 <?php
 require_once "../../classes/User.php";
-// require_once "../../config/functions.php";
-// checkAdmin();
+require_once "../../config/functions.php";
+checkAdmin();
 $userObj = new User();
 
 $errors = [];
