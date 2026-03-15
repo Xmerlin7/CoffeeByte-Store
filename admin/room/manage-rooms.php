7ee03abd9a067915e131a578562f4ef3c248a71a
Xmerlin7 <seifahmedjr7@gmail.com>
2026-03-15 22:45:40 +0200

    Add SCSS for user editing and room management components
    - Introduced `edit_users.scss` for styling the user editing form with a clean layout and responsive design.
    - Added `manage_rooms.scss` for managing room listings, including styles for buttons and room information display.
    - Updated `app.scss` to import the new components for user editing and room management.

--- admin/room/manage-rooms.php
+++ admin/room/manage-rooms.php
@@ -5,24 +5,72 @@ require_once "../../classes/Room.php";
 $roomObj = new Room();
 $rooms = $roomObj->getAllRooms();
 ?>
+<?php
+$title = "CoffeeByte - Manage Rooms";
+ob_start();
+?>
+<div class="rooms-page">
 
-<a href="add-room.php">+ Add Room</a>
+<a href="add-room.php" class="btn-primary add-room-btn">
++ Add Room
+</a>
 
-<table border="1">
-    <tr>
-        <th>ID</th>
-        <th>Room Number</th>
-        <th>Actions</th>
-    </tr>
+<div class="table-wrapper card">
 
-    <?php foreach ($rooms as $room): ?>
-    <tr>
-        <td><?= $room['id'] ?></td>
-        <td><?= $room['room_number'] ?></td>
-        <td>
-            <a href="edit-room.php?id=<?= $room['id'] ?>">Edit</a>
-            <a href="delete-room.php?id=<?= $room['id'] ?>">Delete</a>
-        </td>
-    </tr>
-    <?php endforeach; ?>
+<table class="admin-table">
+
+<tr>
+<th>ID</th>
+<th>Room Number</th>
+<th>Actions</th>
+</tr>
+
+<?php foreach ($rooms as $room): ?>
+<tr>
+
+<td>
+<span class="room-id">
+#<?= $room['id'] ?>
+</span>
+</td>
+
+<td>
+<span class="room-number">
+<?= htmlspecialchars($room['room_number']) ?>
+</span>
+</td>
+
+<td>
+
+<div class="actions">
+
+<a
+href="edit-room.php?id=<?= $room['id'] ?>"
+class="btn-edit"
+>
+Edit
+</a>
+
+<a
+href="delete-room.php?id=<?= $room['id'] ?>"
+class="btn-delete"
+>
+Delete
+</a>
+
+</div>
+
+</td>
+
+</tr>
+<?php endforeach; ?>
+
 </table>
+
+</div>
+
+</div>
+<?php
+$content = ob_get_clean();
+include "../../layouts/dash.php";
+?>
