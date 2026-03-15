1d8ad8b9cf0b1eb87ed374e90af4be603910af4e
Xmerlin7 <seifahmedjr7@gmail.com>
2026-03-16 01:15:18 +0200

    fix: enable admin check in manage-users.php

--- admin/user/manage-users.php
+++ admin/user/manage-users.php
@@ -1,7 +1,7 @@
 <?php
 require_once "../../classes/User.php";
 require_once "../../config/functions.php";
-// checkAdmin();
+checkAdmin();
 $userObj = new User();
 $users = $userObj->getAllUsers();
 ?>
